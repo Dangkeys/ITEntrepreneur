87e47efc5beeb3e5c79eca8a641cabc3cad31aac
billza555 <nphisu@gmail.com>
2025-01-25 09:54:36 +0700

    feat: update check triangle

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ function TriangleIdentify(side1, side2, side3) {
   if (side1 !== side2 && side2 !== side3 && side1 !== side3)
     return { result: "Scalene Triangle/สามเหลี่ยมด้านไม่เท่า" };
   if (
-    side1 ^ (2 === side2) ^ (2 + side3) ^ 2 ||
-    side2 ^ (2 === side1) ^ (2 + side3) ^ 2 ||
-    side3 ^ (2 === side1) ^ (2 + side2) ^ 2
+    (side1 ^ 2) === (side2 ^ 2) + (side3 ^ 2) ||
+    (side2 ^ 2) === (side1 ^ 2) + (side3 ^ 2) ||
+    (side3 ^ 2) === (side1 ^ 2) + (side2 ^ 2)
   )
     return { result: "Right Triangle/สามเหลี่ยมมุมฉาก" };
   if (
-    side1 ^ (2 < side2) ^ (2 + side3) ^ 2 ||
-    side2 ^ (2 < side1) ^ (2 + side3) ^ 2 ||
-    side3 ^ (2 < side1) ^ (2 + side2) ^ 2
+    (side1 ^ 2) < (side2 ^ 2) + (side3 ^ 2) ||
+    (side2 ^ 2) < (side1 ^ 2) + (side3 ^ 2) ||
+    (side3 ^ 2) < (side1 ^ 2) + (side2 ^ 2)
   )
     return { result: "Acute Triangle/สามเหลี่ยมมุมแหลม" };
   if (
-    side1 ^ (2 > side2) ^ (2 + side3) ^ 2 ||
-    side2 ^ (2 > side1) ^ (2 + side3) ^ 2 ||
-    side3 ^ (2 > side1) ^ (2 + side2) ^ 2
+    (side1 ^ 2) < (side2 ^ 2) + (side3 ^ 2) ||
+    (side2 ^ 2) < (side1 ^ 2) + (side3 ^ 2) ||
+    (side3 ^ 2) < (side1 ^ 2) + (side2 ^ 2)
   )
     return { result: "Obtuse Triangle/สามเหลี่ยมมุมป้าน" };
   return { exception: "Triangle type doesn't match" };
